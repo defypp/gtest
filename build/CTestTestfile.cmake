bd67a905b237559638614f51aa06c9cb6ac6f1f7
defypp <982994523@qq.com>
2022-09-28 16:29:55 +0800

    create gtest demo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/robot/dev/proj/CPP/gtest_demo/demo
-# Build directory: /media/robot/dev/proj/CPP/gtest_demo/demo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
